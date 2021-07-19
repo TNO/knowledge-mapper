329b461197e5552617cc6df9464e40af9491ce53
Han Kruiger <han.kruiger@tno.nl>
2021-07-19 10:07:44 +0200

    Make the plugin example work.

--- src/plugins/example.py
+++ src/plugins/example.py
@@ -1,5 +1,16 @@
 from data_sources import DataSource
 
 class ExampleDataSource(DataSource):
+    def __init__(self, example):
+        self.example = example
+
     def test(self):
         print('testing example')
+
+    def handle(self, ki, binding_set):
+        result_bindings = []
+        binding = dict()
+        for variable in ki['vars']:
+            binding[variable] = f'<{self.example}>'
+        result_bindings.append(binding)
+        return result_bindings
