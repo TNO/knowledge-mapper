f0af7e528a247f2cfc707f662ea2b7c184f83a9b
Han Kruiger <han.kruiger@tno.nl>
2021-08-27 15:12:35 +0200

    Add support for inserting in SPARQL

--- src/sparql_source.py
+++ src/sparql_source.py
@@ -13,20 +13,36 @@ class SparqlSource(DataSource):
 
     def test(self):
         log.info('Testing SPARQL endpoint.')
-        self.do_sparql('SELECT * WHERE { ?s ?p ?o . } LIMIT 0')
+        self.do_sparql_select('SELECT * WHERE { ?s ?p ?o . } LIMIT 0')
         log.info('Succes!')
 
 
     def handle(self, ki, binding_set):
+        if ki['type'] == 'answer':
+            return self.handle_answer(ki, binding_set)
+        elif ki['type'] == 'react':
+            return self.handle_react(ki, binding_set)
+
+
+    def handle_answer(self, ki, binding_set):
         # Generate the SPARQL query based on the incoming bindings and the knowledge interaction's graph pattern.
-        query = generate_sparql(ki, binding_set)
+        query = generate_sparql_select(ki, binding_set)
         # Fire the SPARQL query.
-        result = self.do_sparql(query)
+        result = self.do_sparql_select(query)
         # Restructure the bindings into TKE bindings and return it
         return restructure_bindings(result)
 
 
-    def do_sparql(self, query):
+    def handle_react(self, ki, binding_set):
+        # Generate the SPARQL query based on the incoming bindings and the knowledge interaction's graph pattern.
+        query = generate_sparql_insert(ki, binding_set)
+        # Fire the SPARQL query.
+        self.do_sparql_insert(query)
+        # Restructure the bindings into TKE bindings and return it
+        return []
+
+
+    def do_sparql_select(self, query):
         args = {
             'headers': {
                 'Accept': 'application/json'
@@ -48,7 +64,33 @@ class SparqlSource(DataSource):
 
         return response.json()
 
-def generate_sparql(ki, incoming_bindings):
+
+    def do_sparql_insert(self, query):
+        args = {
+            'headers': {
+                'Accept': 'application/json',
+                'Content-Type': 'application/sparql-update'
+            }
+        }
+
+        if 'SPARQL_USERNAME' in os.environ and 'SPARQL_PASSWORD' in os.environ:
+            args['auth'] = HTTPBasicAuth(os.environ['SPARQL_USERNAME'], os.environ['SPARQL_PASSWORD'])
+
+        response = requests.post(
+            f'{self.endpoint}',
+            data=query,
+            **args
+        )
+
+        if response.status_code == 401:
+            raise UnauthorizedError("Provide BasicAuth with system environment variables SPARQL_USERNAME and SPARQL_PASSWORD.")
+        elif not response.ok:
+            raise RuntimeError("Invalid response from SPARQL endpoint.  (status: {}, body: {})".format(response.status_code, response.text))
+
+        return
+
+
+def generate_sparql_select(ki, incoming_bindings):
     # For all partial bindings that are actually partial, we set the
     # variables that ARE in the graph pattern, but NOT in the binding to
     # UNDEF, so that they match anything.
@@ -76,6 +118,17 @@ def generate_sparql(ki, incoming_bindings):
         ).format(
             variables=' '.join([f'?{var}' for var in ki['vars']]),
         )
+
+
+def generate_sparql_insert(ki, bindings):
+    variables = ki['vars']
+    return f'''
+        INSERT {{
+            {ki['argument_pattern']}
+        }} WHERE {{ VALUES ({' '.join([f'?{variable}' for variable in variables])}) {{
+            {os.linesep.join(f'({" ".join([str(binding[variable]) for variable in variables])})' for binding in bindings)}
+        }} }}
+    '''
 
 
 def restructure_bindings(sparql_results):
