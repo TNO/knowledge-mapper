738cabfba7ba746fe0d0cad18b9e846e1d14167b
Han Kruiger <han.kruiger@tno.nl>
2021-10-04 16:08:40 +0200

    rename sc service to tke-runtime in docker-compose

--- knowledge_mapper/data_source.py
+++ knowledge_mapper/data_source.py
@@ -3,5 +3,6 @@ class DataSource:
         raise NotImplementedError("Please implement this abstract method.")
     def handle(self, ki, binding_set):
         raise NotImplementedError("Please implement this abstract method.")
+
     def set_tke_client(self, tke_client):
         self.tke_client = tke_client
