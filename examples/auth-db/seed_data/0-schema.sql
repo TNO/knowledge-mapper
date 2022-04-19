bc334dd5d0fe28462646261142b9705d5fef41d5
Han Kruiger <han.kruiger@tno.nl>
2022-04-19 17:17:35 +0200

    Allow nullable knowledge_interaction_id, interpreted as a wildcard.

--- examples/auth-db/seed_data/0-schema.sql
+++ examples/auth-db/seed_data/0-schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE knowledge_interactions (
 
 CREATE TABLE policies (
   id INT NOT NULL AUTO_INCREMENT,
-  knowledge_interaction_id VARCHAR(512) NOT NULL,
+  knowledge_interaction_id VARCHAR(512),
   knowledge_base_id VARCHAR(512),
   PRIMARY KEY(id),
   FOREIGN KEY (knowledge_interaction_id) REFERENCES knowledge_interactions(id)
