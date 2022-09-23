0d72d1915ba879a5c3462e7363ae121860910fe6
Han Kruiger <han.kruiger@tno.nl>
2022-09-23 17:16:14 +0200

    add catchall for exceptions in the handle method, and return n empty binding set if so

--- examples/custom-mapper/example.py
+++ examples/custom-mapper/example.py
@@ -1,17 +1,18 @@
 from knowledge_mapper.data_source import DataSource
 
+
 class ExampleDataSource(DataSource):
     def __init__(self, argument):
         print(argument)
 
     def test(self):
-        print('testing example')
+        print("testing example")
 
     def handle(self, ki, binding_set, requesting_kb):
         result_bindings = []
         binding = dict()
-        binding['tree'] = '<http://example.org/maple>'
-        binding['height'] = '"44"^^<http://www.w3.org/2001/XMLSchema#integer>'
-        binding['name'] = '"Maple"'
+        binding["tree"] = "<http://example.org/maple>"
+        binding["height"] = '"44"^^<http://www.w3.org/2001/XMLSchema#integer>'
+        binding["name"] = '"Maple"'
         result_bindings.append(binding)
         return result_bindings
