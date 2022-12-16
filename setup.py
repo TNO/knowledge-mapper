d13d36b676454655622fa08f094604abb5c7269a
Han Kruiger <han.kruiger@tno.nl>
2022-12-16 15:48:48 +0100

    version 0.0.20

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="knowledge_mapper",
-    version="0.0.19",
+    version="0.0.20",
     packages=find_packages(),
     install_requires=["requests", "mysql-connector-python", "json5"],
     entry_points={
