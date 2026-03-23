a8c2fca6492731a29196d54773425eeab710fa56
David de Best <david.debest@tno.nl>
2026-03-23 15:21:31 +0100

    Update version to 0.1.0a0 and init uv project

--- mapper/setup.py
+++ mapper/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="knowledge_mapper",
-    version="0.0.24",
+    version="0.1.0a0",
     packages=find_packages(),
     install_requires=[
         "requests",
